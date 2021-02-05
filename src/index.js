8ab65abbf92417c3488aa711e301cee9ead0b5d8
marynaciubrei <marinaciubrei@gmail.com>
2021-02-05 12:44:22 +0200

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array||array.length==0){
+    return 0
+  }else{
+    return array.sort((a,b)=>a - b)[0]
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array||array.length==0){
+    return 0
+  }else{
+    return array.sort((a,b)=>b-a)[0]
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array||array.length==0){
+    return 0
+  }else{
+    return array.reduce((a,b)=>a+b)/array.length
+  }
 }
